a6fedd5c1811e3759d7ef7bc4bc68daabc97ade6
Xingchen Lin <FutureCoder@aliyun.com>
2026-03-23 17:17:14 +0800

    add request sample log

--- sample/l7http/hello/main.go
+++ sample/l7http/hello/main.go
@@ -34,5 +34,37 @@ func main() {
 
 	addr := "127.0.0.1:" + *port
 	log.Printf("[%s] listening on %s", *name, addr)
+	// 每秒钟打印一条虚拟访问日志，包含参数
+	go func(service string) {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+
+		paths := []string{"/", "/healthz", "/docs"}
+		seq := 1
+		for now := range ticker.C {
+			payload, err := json.Marshal(map[string]any{
+				"type":       "virtual_request",
+				"service":    service,
+				"request_id": seq,
+				"method":     "GET",
+				"path":       paths[(seq-1)%len(paths)],
+				"query": map[string]any{
+					"demo":   "log-panel",
+					"seq":    seq,
+					"source": "sample/l7http/hello",
+				},
+				"user_agent": "GopherMeshDocDemo/1.0",
+				"timestamp":  now.UTC().Format(time.RFC3339Nano),
+			})
+			if err != nil {
+				log.Printf("[%s] virtual_request marshal failed: %v", service, err)
+				continue
+			}
+
+			log.Printf("[%s] %s", service, payload)
+			seq++
+		}
+	}(*name)
+
 	log.Fatal(http.ListenAndServe(addr, mux))
 }
